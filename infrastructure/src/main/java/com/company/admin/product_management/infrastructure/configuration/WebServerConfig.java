ea2ca320b45aed0fe001f3248554fe7f8709f2dc
rafael dos santos <rafael-dos-santos@hotmail.com>
2024-05-04 17:56:32 -0300

    feat: install springboot

---
+++ infrastructure/src/main/java/com/company/admin/product_management/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.company.admin.product_management.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.company.admin.product_management")
+public class WebServerConfig {
+}
